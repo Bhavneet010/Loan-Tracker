a7a3b9b475530c3eb33e6e58289fa6e2230ca902
Bhavneet010 <bhavneetsingh123456@gmail.com>
2026-04-27 19:33:05 +0530

    Center splash wordmark

--- sw.js
+++ sw.js
@@ -22,7 +22,7 @@ messaging.onBackgroundMessage((payload) => {
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-const CACHE = 'nirnay-v47';
+const CACHE = 'nirnay-v48';
 const ASSETS = [
   './',
   './index.html',
