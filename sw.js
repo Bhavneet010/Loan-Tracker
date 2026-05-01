1b91d29378cfde51b4e3fb8d965bcb2fb8b47cd3
Bhavneet010 <bhavneetsingh123456@gmail.com>
2026-05-01 19:13:27 +0530

    Bump service worker cache version

--- sw.js
+++ sw.js
@@ -22,7 +22,7 @@ messaging.onBackgroundMessage((payload) => {
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-const CACHE = 'nirnay-v49';
+const CACHE = 'nirnay-v50';
 const ASSETS = [
   './',
   './index.html',
